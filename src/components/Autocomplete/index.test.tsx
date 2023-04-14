2ec14566e3683bfbae5634ee83cb3cec2820d3ab
agostinhodev <agostinho.developer@gmail.com>
2023-04-13 21:44:26 -0300

    feat: :sparkles: updated test

--- src/components/Autocomplete/index.test.tsx
+++ src/components/Autocomplete/index.test.tsx
@@ -1,9 +1,36 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import axios from 'axios';
 
 import Autocomplete from '.';
 
-test('renders input element', () => {
-  const { getByPlaceholderText } = render(<Autocomplete />);
-  const inputElement = getByPlaceholderText(/please enter the name of the country here/i);
-  expect(inputElement).toBeInTheDocument();
+jest.mock('axios');
+
+describe('Autocomplete Component', () => {
+  it('should render the input', () => {
+    render(<Autocomplete />);
+    expect(
+      screen.getByPlaceholderText(/please enter the name of the country here/i),
+    ).toBeInTheDocument();
+  });
+
+  it('should show country list when input has value', async () => {
+    const mockResponse = {
+      data: [
+        { name: 'Canada', code: 'CA' },
+        { name: 'United States', code: 'US' },
+      ],
+    };
+    axios.get = jest.fn().mockResolvedValueOnce(mockResponse);
+
+    render(<Autocomplete />);
+    const inputElement = screen.getByPlaceholderText(/please enter the name of the country here/i);
+
+    fireEvent.change(inputElement, { target: { value: 'ca' } });
+
+    const listItem1 = await screen.findByText('Canada');
+    const listItem2 = await screen.findByText('United States');
+
+    expect(listItem1).toBeInTheDocument();
+    expect(listItem2).toBeInTheDocument();
+  });
 });
