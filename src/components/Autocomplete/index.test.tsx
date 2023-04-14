68bac59fcf2232e72fcb8b4e2df46a1b1d9bdfb2
agostinhodev <agostinho.developer@gmail.com>
2023-04-13 21:29:55 -0300

    test: :white_check_mark: added rendering text

--- src/components/Autocomplete/index.test.tsx
+++ src/components/Autocomplete/index.test.tsx
@@ -1,10 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import Autocomplete from '.';
 
-describe('Text', () => {
-  it('should render', () => {
-    render(<Autocomplete />);
-    expect(screen.getByRole('heading', { name: /Boas vindas/i })).toBeInTheDocument();
-  });
+test('renders input element', () => {
+  const { getByPlaceholderText } = render(<Autocomplete />);
+  const inputElement = getByPlaceholderText(/please enter the name of the country here/i);
+  expect(inputElement).toBeInTheDocument();
 });
